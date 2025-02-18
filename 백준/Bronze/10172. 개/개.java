17d357a0d6e77b1c46ced853c74a41aedd213546
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-02-18 21:28:25 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "|\\_/|\n";
+        str += "|q p|   /}\n";
+        str += "( 0 )\"\"\"\\\n";
+        str += "|\"^\"`    |\n";
+        str += "||_/=\\\\__|";
+        System.out.println(str);
+    }
+}
