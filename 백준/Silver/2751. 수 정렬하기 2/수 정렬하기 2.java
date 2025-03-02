ec94aea15bf513d8f9507ad6623fc5b1de612475
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-03-02 16:28:21 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1524 ms, Memory: 209700 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,19 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.TreeSet;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            TreeSet<Integer> numbers = new TreeSet<>();
+            Set<Integer> numbers = new HashSet<>();
             int count = Integer.parseInt(br.readLine());
             for (int i = 0; i < count; i++) {
                 numbers.add(Integer.parseInt(br.readLine()));
             }
+            
             StringBuilder sb = new StringBuilder();
-
+            
             numbers.stream()
+                    .sorted(Comparator.naturalOrder())
                     .forEach(n -> sb.append(n).append(System.lineSeparator()));
 
             System.out.println(sb.toString().trim());
