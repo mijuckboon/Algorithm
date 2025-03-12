1493178e18276ef64fabe93430010cef6d3f9d29
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-03-12 15:49:25 +0900

    [Silver I] Title: 신입 사원, Time: 764 ms, Memory: 300996 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,41 +1,37 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main { // 확인용 GPT 코드
     static final StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-        final int numTests = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        
-        for (int i = 0; i < numTests; i++) {
-            int numApplies = Integer.parseInt(br.readLine());
-            Integer[][] ranks = new Integer[numApplies][2]; 
-            for (int j = 0; j < numApplies; j++) {
-                st = new StringTokenizer(br.readLine());
-                ranks[j][0] = Integer.parseInt(st.nextToken());
-                ranks[j][1] = Integer.parseInt(st.nextToken());
-            }
-            
-            PriorityQueue<Integer[]> pq = new PriorityQueue<>(Comparator.comparingInt(rank -> rank[0]));
-            for (int j = 0; j < numApplies; j++) {
-                pq.offer(ranks[j]);
-            }
-            
-            int minInterviewRank = Integer.MAX_VALUE;
-            int interviewRank;
-            int count = 0;
-            for (int j = 0; j < numApplies; j++) {
-                interviewRank = pq.poll()[1];
-                if (minInterviewRank > interviewRank) {
-                    minInterviewRank = interviewRank;
-                    count += 1;
+            final int numTests = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < numTests; i++) {
+                int numApplies = Integer.parseInt(br.readLine());
+                int[] interviewRanks = new int[numApplies + 1]; // 서류 심사 등수 -> 면접 등수를 저장하는 배열
+                
+                for (int j = 0; j < numApplies; j++) {
+                    StringTokenizer st = new StringTokenizer(br.readLine());
+                    int docRank = Integer.parseInt(st.nextToken());
+                    int interviewRank = Integer.parseInt(st.nextToken());
+                    interviewRanks[docRank] = interviewRank; // 정렬 없이 직접 저장
                 }
+
+                int minInterviewRank = Integer.MAX_VALUE;
+                int count = 0;
+
+                for (int j = 1; j <= numApplies; j++) { // 1등부터 차례대로 확인
+                    if (interviewRanks[j] < minInterviewRank) {
+                        minInterviewRank = interviewRanks[j];
+                        count++;
+                    }
+                }
+
+                sb.append(count).append("\n");
             }
-            sb.append(count).append(System.lineSeparator());
         }
-        System.out.println(sb.toString().trim());
+        System.out.print(sb);
     }
-}
 }
