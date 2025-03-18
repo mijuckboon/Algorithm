16fec46238e594d66debb60669a60cf450f654c6
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-03-18 23:33:12 +0900

    [level 2] Title: 올바른 괄호, Time: 16.39 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,22 +2,19 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {     
+
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
                 stack.push('(');
             } else {
                 if (stack.isEmpty()) {
                     return false;
-                } else {
-                    stack.pop();
                 }
+                    stack.pop();
             }
-            
-        }
-        if (stack.isEmpty()) return true;
 
-        return false;
+        }
+        return stack.isEmpty();
     }
 }
