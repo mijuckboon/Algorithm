30422212889a895494361cdd8dad0ea256be92b1
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-03-19 22:18:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 71.23 ms, Memory: 65.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        for (char ch: s.toCharArray()) {
+            if (stack.isEmpty() || stack.peek() != ch) {
+                stack.push(ch);
+            } else {
+                stack.pop();
+            }
+        }
+        
+        
+        return stack.isEmpty()? 1 : 0;
+    }
+}
