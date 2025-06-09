ed33fceca27dce8efad38922d318fa05f364c487
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-06-09 23:04:26 +0900

    [level 2] Title: N-Queen, Time: 61.61 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -13,9 +13,10 @@ class Solution {
     ) {
         int count = 0;
         if (row == n) {// 모든 행에 대해 채워진 경우
-            count += 1; // 경우의 수 카운터에 1 추가
+            return 1;
+            // count += 1; // 경우의 수 카운터에 1 추가
         }
-        else {
+        // else { <- 하단 풀이 보고 수정
             for (int col = 0; col < n; col++) {
                 // 해당 열 또는 대각선에 이미 퀸이 있는 경우
                 if (hasQueenInCol[col] || hasQueenInUpwardDiag[col + row] || hasQueenInDownwardDiag[n - row + col]) {
@@ -37,9 +38,43 @@ class Solution {
                 
             }
             
-        }
+        // }
         return count;
         
         
     }
 }
+
+/* Min Ho Pak님 깔끔한 풀이 */
+// public int nQueen(int n) {
+//         int result = 0;
+//         int[] cols = new int[n];
+//         result = backTrack(0, cols, n);
+
+//         return result;
+//     }
+
+//     public int backTrack(int level, int[] cols, int n) {
+//         int sum = 0;
+//         if (level == n) {
+//             return 1;
+//         } else {
+//             for (int i = 0; i < n; i++) {
+//                 cols[level] = i;
+//                 if (possible(level, cols)) {
+//                     sum += backTrack(level + 1, cols, n);
+//                 }
+//             }
+//         }
+//         return sum;
+//     }
+
+
+//     public boolean possible(int level, int[] cols) {
+//         for (int i = 0; i < level; i++) {
+//             if (cols[i] == cols[level] || Math.abs(level - i) == Math.abs(cols[i] - cols[level])) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
