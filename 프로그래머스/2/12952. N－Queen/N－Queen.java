9749497d0b0bb7629321fbeebb44627a214404a4
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-06-09 22:55:16 +0900

    [level 2] Title: N-Queen, Time: 60.19 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int solution(int n) {
+        boolean[] hasQueenInCol = new boolean[n];
+        boolean[] hasQueenInUpwardDiag = new boolean[2 * n]; // 우상향
+        boolean[] hasQueenInDownwardDiag = new boolean[2 * n]; // 우하향
+        
+        return countCases(0, n, hasQueenInCol, hasQueenInUpwardDiag, hasQueenInDownwardDiag);
+    }
+    
+    private int countCases(
+        int row, int n, boolean[] hasQueenInCol,
+        boolean[] hasQueenInUpwardDiag, boolean[] hasQueenInDownwardDiag
+    ) {
+        int count = 0;
+        if (row == n) {// 모든 행에 대해 채워진 경우
+            count += 1; // 경우의 수 카운터에 1 추가
+        }
+        else {
+            for (int col = 0; col < n; col++) {
+                // 해당 열 또는 대각선에 이미 퀸이 있는 경우
+                if (hasQueenInCol[col] || hasQueenInUpwardDiag[col + row] || hasQueenInDownwardDiag[n - row + col]) {
+                    continue;
+                }
+                
+                // 퀸 배치
+                hasQueenInCol[col] = true;
+                hasQueenInUpwardDiag[col + row] = true;
+                hasQueenInDownwardDiag[n - row + col] = true;
+                
+                count += countCases(row + 1, n, hasQueenInCol, hasQueenInUpwardDiag, hasQueenInDownwardDiag);
+                
+                // 퀸 제거
+                hasQueenInCol[col] = false;
+                hasQueenInUpwardDiag[col + row] = false;
+                hasQueenInDownwardDiag[n - row + col] = false;
+                
+                
+            }
+            
+        }
+        return count;
+        
+        
+    }
+}
