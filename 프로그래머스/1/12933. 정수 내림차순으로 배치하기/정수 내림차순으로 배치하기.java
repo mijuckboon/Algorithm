aa131f43a4d3e8ec2a21c065cca8aab15865a265
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-06-15 16:23:29 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 11.22 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(long n) {
+        String input = Long.toString(n);
+        int[] count = new int[10];
+        
+        for (int i = 0; i < input.length(); i++) {
+            int digit = Integer.parseInt(input.charAt(i) + "");
+            count[digit] += 1;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 9; i >= 0; i--) {
+            sb.append(Integer.toString(i).repeat(count[i])); 
+        }
+        
+        return Long.parseLong(sb.toString());
+    }
+}
