a345075258ac4ea310da41a77a75207b8f9a4044
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-06-18 00:46:52 +0900

    [level 2] Title: 튜플, Time: 49.07 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(String s) {
+        String[] sets = s.substring(2, s.length() - 2) // 양 끝 중괄호 제거 (2개씩)
+            .split("\\},\\{"); // regex 
+        
+//         List<String[]> braceRemovedSets = Arrays.stream(sets)
+//             .map(set -> set.split(","))
+//             .collect(Collectors.toList());
+        
+//         braceRemovedSets.sort(Comparator.comparingInt(x -> x.length));
+        
+        List<List<String>> setList = Arrays.stream(sets)
+            .map(str -> new ArrayList<>(Arrays.asList(str.split(","))))
+            .sorted(Comparator.comparingInt(x -> x.size()))
+            // .map(str -> new HashSet<>(Arrays.asList(str.split(","))))
+            // .sorted(Comparator.comparingInt(Set::size))
+            .collect(Collectors.toList());
+        
+        Set<String> seen = new HashSet<>();
+        List<Integer> tuple = new ArrayList<>();
+        
+        for (List<String> currentSet : setList) {
+            currentSet.removeAll(seen); // 이미 본 숫자 제거
+            String found = currentSet.get(0);
+            // String found = currentSet.stream().findFirst().orElse(null); // List<String> 대신 Set<String>일 때
+            seen.add(found);
+            tuple.add(Integer.parseInt(found));
+        }
+        
+        return tuple.stream().mapToInt(x -> x).toArray();
+    }
+}
