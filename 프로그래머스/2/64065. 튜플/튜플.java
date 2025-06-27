66648cb3b9fb27f36866242a5c2e00dfc365539d
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-06-27 23:09:03 +0900

    [level 2] Title: 튜플, Time: 59.71 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -5,6 +5,7 @@ class Solution {
     public int[] solution(String s) {
         String[] sets = s.substring(2, s.length() - 2) // 양 끝 중괄호 제거 (2개씩)
             .split("\\},\\{"); // regex
+        // System.out.println(Arrays.toString(sets));
         
         List<List<String>> setList = Arrays.stream(sets)
             .map(str -> new ArrayList<>(Arrays.asList(str.split(","))))
@@ -51,5 +52,30 @@ class Solution {
         }
 
         return tuple.stream().mapToInt(i -> i).toArray();
+    }
+    // 첫 시도
+    public int[] firstTrial(String s) {
+        String[] sets = s.substring(1, s.length() - 1) // 양 끝 중괄호 제거
+            .split(",");
+        System.out.println(Arrays.toString(sets));
+        // 출력: [{2}, {2, 1}, {2, 1, 3}, {2, 1, 3, 4}]
+        // 실제로는 "{2}", "{2", "1}", ... 과 같이 잘못 잘린 형태
+        
+        List<String> braceRemovedSets = Arrays.stream(sets)
+            .map(set -> {
+                int len = set.length();
+                // if (len >= 2) { <- 이 부분이 없으면 (있으면 empty string이 많은 리스트)
+                    return set.substring(1, len - 1); // 여기서 exception 터짐 
+                // }
+                // return set;
+            })
+            // .map(set -> set.split(","))
+            // .sort(Comparator.comparingInt((String[] x) -> x.length))
+            .collect(Collectors.toList());
+        System.out.println(braceRemovedSets);
+        
+        int[] answer = {};
+        return answer;
+        
     }
 }
