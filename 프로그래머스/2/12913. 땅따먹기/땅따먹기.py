3b022197cc0ead6f67ffcc2cbb18eb5fa96d9429
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-09-23 21:32:19 +0900

    [level 2] Title: 땅따먹기, Time: 355.44 ms, Memory: 19.7 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -3,6 +3,6 @@ def solution(land):
     for row in land[1:]:
         tmp = [0] * 4
         for i in range(4):
-            tmp[i] = max(dp[(i + 1) % 4], dp[(i + 2) % 4], dp[(i + 3) % 4]) + row[i]
+            tmp[i] = max(dp[j] for j in range(4) if j != i) + row[i]
         dp = tmp
     return max(dp)
