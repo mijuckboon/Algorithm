787678dbe89e9e98c11d6e57445426169214661f
Jinwoong Kwak <91645305+mijuckboon@users.noreply.github.com>
2025-09-29 02:06:58 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int len = num_list.length;
+        
+        int[] reversed = new int[len];
+        for (int i = 0; i < len; i++) {
+            reversed[i] = num_list[len - i - 1];
+        }
+        
+        return reversed;
+    }
+}
